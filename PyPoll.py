24bf72bba0297b495bc27b70204cce82ca39ae27
crkaide <crkaide@ilstu.edu>
2021-03-26 22:19:11 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -8,10 +8,6 @@
 # Resources/election_results.csv
 # /Users/crkaide/OneDrive - IL State University/Vanderbilt/Vanderbilt/Assignments/3_Python/Election_Analysis/Resources/election_results.csv
 
-
-
-
-
 # # Assign a variable for the file to load and the path.
 # file_to_load = '/Users/crkaide/OneDrive - IL State University/Vanderbilt/Vanderbilt/Assignments/Three_3_Python/Election_Analysis/Resources/election_results.csv'
 
@@ -20,6 +16,14 @@
 
 #      # To do: perform analysis.
 #      print(election_data)
+
+# # Add the filename variable that references the path to election_results.csv.
+# file_to_load = '/Users/crkaide/OneDrive - IL State University/Vanderbilt/Vanderbilt/Assignments/3_Python/Election_Analysis/Resources/election_results.csv'
+
+# # Create a filename variable to the path for writing results.
+# file_to_save = '/Users/crkaide/OneDrive - IL State University/Vanderbilt/Vanderbilt/Assignments/3_Python/Election_Analysis/Analysis/election_analysis.txt'
+
+
 
 
 
@@ -35,17 +39,76 @@ file_to_save = '/Users/crkaide/OneDrive - IL State University/Vanderbilt/Vanderb
 
 
 
-# Open the election_results.csv using the with statement as the filename object, election_data.
-with open(file_to_load) as election_data:
+# To count up all the votes, we need to initialize a variable, which is called an accumulator, that will increment by 1 as we read each row in the for loop. For convenience, we will initialize a variable called total_votes to zero.
 
-    #To do:  read and analyze the data here.
+# The total_votes variable needs to be placed above the code where we open the file, using the with open() statement. We do this because every time we run the file, the total_votes variable must be set equal to zero.
+
+
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate options and candidate votes
+candidate_options = []
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # # Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row)
-
-
-    #Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+          # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+           # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Tally votes for each candidate
+        candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #  To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         # If true then set winning_count = votes and winning_percent =
+         # vote_percentage.
+         winning_count = votes
+         winning_percentage = vote_percentage
+         # And, set the winning_candidate equal to the candidate's name.
+         winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
